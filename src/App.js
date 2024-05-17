6e28650e755c2a073ccf98a110e2efd2a29873fa
waleed1123 <waleed007896@gmail.com>
2024-05-18 02:45:15 +0500

    Todo Applicaition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // Declare state variables inside the component
+  const [txt, setTxt] = useState('App');
+  const [txtList, setTxtList] = useState([]);
+
+  // Delete function
+  const del = (i) => {
+    txtList.splice(i, 1);
+    setTxtList([...txtList]);
+  };
+
+  // Edit function
+  const edit = (i) => {
+    let newVal = prompt('Enter New Value');
+    console.log(newVal);
+    txtList.splice(i, 1, newVal);
+    setTxtList([...txtList]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <input
+          className="input"
+          type="text"
+          onChange={(e) => {
+            setTxt(e.target.value);
+          }}
+        ></input>
+        <button
+          className="addTodo"
+          onClick={() => {
+            setTxtList([...txtList, txt]);
+            console.log(txtList);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Add
+        </button>
+        <button
+          className="deleteAll"
+          onClick={() => {
+            setTxtList([]);
+          }}
+        >
+          Delete All
+        </button>
+        {txtList.map((x, i) => {
+          return (
+            <p className="todoPara" key={i}>
+              {x}{" "}
+              <button className="deleteBtn" onClick={() => del(i)}>
+                Delete
+              </button>
+              <button onClick={() => edit(i)} className="editBtn">
+                Edit
+              </button>
+            </p>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
